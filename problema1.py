e9c5a61cb455ff96f37616a5b3e8ccbd966ea906
Dyfog <111368031+Dyfog@users.noreply.github.com>
2023-08-29 09:35:51 -0400

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -5,10 +5,34 @@
 #La salida debe ser una tupla en el orden t2 t1 t2
 #---------------------------------
 #Ejemplo de entrada:
-#         20 90 hola
-#		  mundo 44
+#20 90 hola
+#mundo 44
 #La salida debe ser
-#         ('mundo', 44, 20, 90, 'hola', 'mundo', 44)
-t = input()
-m = input()
-print(m)
+#('mundo', 44, 20, 90, 'hola', 'mundo', 44)
+entrada1 = input().split()
+entrada2 = input().split()
+
+t1 = []
+for item in entrada1:
+    if item.isdigit():
+        t1.append(int(item))
+        continue
+    if item.isdecimal():
+        t1.append(float(item))
+        continue
+    else:
+        t1.append(item)
+
+t2 = []
+for item in entrada2:
+    if item.isdigit():
+        t2.append(int(item))
+        continue
+    if item.isdecimal():
+        t2.append(float(item))
+        continue
+    else:
+        t2.append(item)
+
+salida = t2 + t1 + t2
+print(salida)
